54611fedca286816caa7f984d4c0809dcde5fb61
nakaji <nakaji126@gmail.com>
2011-06-29 00:55:40 +0900

    テストプロジェクトを追加
    [.Net]MVVMに触れてみた(2) のエントリ内容に対応

---
+++ MVVMSampleTest/BmiViewModelTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MVVMSample;
+using NUnit.Framework;
+
+namespace MVVMSampleTest
+{
+    [TestFixture]
+    public class BmiViewModelTest
+    {
+        [Test]
+        public void 正常()
+        {
+            var viewModel = new BmiViewModel();
+
+            viewModel.Height = 172;
+            viewModel.Weight = 66;
+
+            Assert.That(viewModel.Bmi, Is.EqualTo(22.30));
+        }
+    }
+}
