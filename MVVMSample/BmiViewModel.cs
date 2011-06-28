3d93847a5e997683e94850bb0541c260676f2cf6
nakaji <nakaji126@gmail.com>
2011-06-29 01:41:53 +0900

    BmiViewModelを導入。
    BMI計算のロジックをBmiViewModelに移動

---
+++ MVVMSample/BmiViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MVVMSample
+{
+    public class BmiViewModel
+    {
+        public double Height { get; set; }
+        
+        public double Weight { get; set; }
+
+        public double Bmi
+        {
+            get
+            {
+                return Math.Truncate(Weight / Math.Pow((Height / 100), 2) * 100) / 100;
+            }
+        }
+    }
+}
--- MVVMSample/MainWindow.xaml.cs
+++ MVVMSample/MainWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace MVVMSample
 {
@@ -26,12 +14,12 @@ namespace MVVMSample
 
         private void calcButton_Click(object sender, RoutedEventArgs e)
         {
-            double height = double.Parse(heightText.Text);
-            double weight = double.Parse(weightText.Text);
+            var viewModel = new BmiViewModel();
 
-            double bmi = Math.Truncate(weight / Math.Pow((height / 100), 2) * 100) / 100;
-            bmiText.Text = string.Format("{0,5:##.##}", bmi);
+            viewModel.Height = double.Parse(heightText.Text);
+            viewModel.Weight = double.Parse(weightText.Text);
 
+            bmiText.Text = string.Format("{0,5:##.##}", viewModel.Bmi);
         }
     }
 }
