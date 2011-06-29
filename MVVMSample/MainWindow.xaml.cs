9d2b285f1c945101443af3521f2ba9f819c91787
nakaji <nakaji126@gmail.com>
2011-06-30 01:03:43 +0900

    UIの変更
    [.Net]MVVMに触れてみた(3)に対応

--- MVVMSample/MainWindow.xaml.cs
+++ MVVMSample/MainWindow.xaml.cs
@@ -14,12 +14,8 @@ namespace MVVMSample
 
         private void calcButton_Click(object sender, RoutedEventArgs e)
         {
-            var viewModel = new BmiViewModel();
-
-            viewModel.Height = double.Parse(heightText.Text);
-            viewModel.Weight = double.Parse(weightText.Text);
-
-            bmiText.Text = string.Format("{0,5:##.##}", viewModel.Bmi);
+            var viewModel = (BmiViewModel) DataContext;
+            viewModel.Calc();
         }
     }
 }
