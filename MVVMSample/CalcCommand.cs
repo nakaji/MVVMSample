837e9911b825394443c65af81185457a3ddc683e
nakaji <nakaji126@gmail.com>
2011-06-30 01:54:39 +0900

    CalcCommandを追加

---
+++ MVVMSample/CalcCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVMSample
+{
+    public class CalcCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
