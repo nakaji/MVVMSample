82e04101889e19180029ec28827943fe50118039
nakaji <nakaji126@gmail.com>
2011-06-30 01:02:46 +0900

    テストケースの変更と追加

--- MVVMSampleTest/BmiViewModelTest.cs
+++ MVVMSampleTest/BmiViewModelTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using MVVMSample;
 using NUnit.Framework;
 
@@ -15,7 +16,26 @@ namespace MVVMSampleTest
             viewModel.Height = 172;
             viewModel.Weight = 66;
 
+            viewModel.Calc();
+
             Assert.That(viewModel.Bmi, Is.EqualTo(22.30));
+        }
+
+        [Test]
+        public void プロパティの変更通知確認()
+        {
+            var viewModel = new BmiViewModel();
+            String propertyName = "";
+
+            viewModel.PropertyChanged += new PropertyChangedEventHandler((s, e) => { propertyName = e.PropertyName; });
+            viewModel.Height = 172;
+            viewModel.Weight = 66;
+
+            Assert.That(propertyName, Is.EqualTo(""));
+
+            viewModel.Calc();
+
+            Assert.That(propertyName, Is.EqualTo("Bmi"));
         }
     }
 }
