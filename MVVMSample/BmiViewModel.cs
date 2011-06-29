13bbc2a5eed79dd2561e24ed543c0177d57b864c
nakaji <nakaji126@gmail.com>
2011-06-30 01:00:46 +0900

    BmiViewModelにINotifyPropertyChangedインターフェイスを実装

--- MVVMSample/BmiViewModel.cs
+++ MVVMSample/BmiViewModel.cs
@@ -1,19 +1,38 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace MVVMSample
 {
-    public class BmiViewModel
+    public class BmiViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged == null) return;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public double Height { get; set; }
-        
+
         public double Weight { get; set; }
 
+        private double _bmi;
         public double Bmi
         {
-            get
+            get { return _bmi; }
+
+            set
             {
-                return Math.Truncate(Weight / Math.Pow((Height / 100), 2) * 100) / 100;
+                _bmi = value;
+                RaisePropertyChanged("Bmi");
             }
+        }
+
+        public void Calc()
+        {
+            //小数第2位で切り捨て
+            Bmi = Math.Truncate((Weight / Math.Pow((Height / 100), 2)) * 100) / 100;
         }
     }
 }
